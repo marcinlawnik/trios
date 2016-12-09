94633f25479eea9a92a904f597b2175d90ae08ce
MichalDolata <dev@dolata.me>
2016-12-09 17:22:27 +0100

    Add single trio route

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,15 @@ Route::get('/dev/playground', function() {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+Route::get('/admin/trios/{id}', function($id) {
+    $trio = Trio::findOrFail($id);
+    return "ID: {$trio->id}<br>
+            Sentence 1: {$trio->sentence1}<br>
+            Sentence 2: {$trio->sentence2}<br>
+            Sentence 3: {$trio->sentence3}<br>
+            Explanation 1: {$trio->explanation1}<br>
+            Explanation 2: {$trio->explanation2}<br>
+            Explanation 3: {$trio->explanation3}<br>
+            Answer: {$trio->answer}<br>
+            Creaton time: {$trio->created_at}";
+});
