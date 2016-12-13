a2b0ba690fdf3359f9aec235b227da59ae9f51f3
MichalDolata <dev@dolata.me>
2016-12-13 19:29:49 +0100

    Add trio editing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@
 
 use Carbon\Carbon;
 use App\Trio;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     $trio = Trio::inRandomOrder()->first();
@@ -29,15 +30,42 @@ Route::get('/dev/playground', function() {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
-Route::get('/admin/trios/{id}', function($id) {
-    $trio = Trio::findOrFail($id);
-    return "ID: {$trio->id}<br>
-            Sentence 1: {$trio->sentence1}<br>
-            Sentence 2: {$trio->sentence2}<br>
-            Sentence 3: {$trio->sentence3}<br>
-            Explanation 1: {$trio->explanation1}<br>
-            Explanation 2: {$trio->explanation2}<br>
-            Explanation 3: {$trio->explanation3}<br>
-            Answer: {$trio->answer}<br>
-            Creaton time: {$trio->created_at}";
+Route::get('/admin/trios/{trio}', function(Trio $trio) {
+    $attributes = $trio->getAttributes();
+    foreach ($attributes as $key => $value) {
+        echo "{$key}: {$value}<br>";
+    }
+    echo "<a href='/admin/trios/{$trio->id}/edit'>Edit trio</a>";
+    return '';
+});
+
+
+Route::get('/admin/trios/{trio}/edit', function(Trio $trio) {
+    $csrf = csrf_field();
+
+    $fillable = $trio->getFillable();
+    $attributes = $trio->getAttributes();
+
+    echo "<form aciton=\'/admin/trios/{$trio->id}/edit\' method='post'>
+                {$csrf}";
+
+    foreach ($fillable as $key) {
+        echo "<input name='{$key}' type='text' value='{$attributes[$key]}'><br>";
+    }
+    echo  "<input type='submit'>
+            </form>";
+    return '';
+});
+
+Route::post('/admin/trios/{trio}/edit', function(Request $request, Trio $trio) {
+    $trio->sentence1 = $request->input('sentence1', $trio->sentence1);
+    $trio->sentence2 = $request->input('sentence2', $trio->sentence2);
+    $trio->sentence3 = $request->input('sentence3', $trio->sentence3);
+    $trio->explanation1 = $request->input('explanation1', $trio->explanation1);
+    $trio->explanation2 = $request->input('explanation2', $trio->explanation2);
+    $trio->explanation3 = $request->input('explanation3', $trio->explanation3);
+    $trio->answer = $request->input('answer', $trio->answer);
+
+    $trio->save();
+    return redirect("/admin/trios/{$trio->id}");
 });
