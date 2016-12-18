0549516e9495247413c7a57e9c33b3786ec11a7e
Michał Dolata <dev@dolata.me>
2016-12-18 13:03:14 +0100

    Merge Michaldolata (#16) - fixes #3 #4 #6
    * Add single trio route
    
    * Add trio editing
    
    * Fix misspell
    
    * Add view for viewing and editing single trio #4 #6
    
    * Naprawa nawiasów #16
    
    * Dodanie dodawania trio #3
    
    * Change path to views to admin/trios/ #3 #4 #6
    
    * Add editing incomplete trio #3

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@
 
 use Carbon\Carbon;
 use App\Trio;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     $trio = Trio::inRandomOrder()->first();
@@ -29,3 +30,49 @@ Route::get('/dev/playground', function() {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('/admin/trios/{trio}', function(Trio $trio) {
+    return view('admin.trios.view')->with('trio', $trio);
+})->where('trio', '[0-9]+');
+
+Route::get('/admin/trios/{trio}/edit', function(Trio $trio) {
+    return view('admin.trios.edit')->with('trio', $trio);
+})->where('trio', '[0-9]+');
+
+Route::post('/admin/trios/{trio}/edit', function(Request $request, Trio $trio) {
+    $trio->sentence1 = $request->input('sentence1', $trio->sentence1);
+    $trio->sentence2 = $request->input('sentence2', $trio->sentence2);
+    $trio->sentence3 = $request->input('sentence3', $trio->sentence3);
+    $trio->explanation1 = $request->input('explanation1', $trio->explanation1);
+    $trio->explanation2 = $request->input('explanation2', $trio->explanation2);
+    $trio->explanation3 = $request->input('explanation3', $trio->explanation3);
+    $trio->answer = $request->input('answer', $trio->answer);
+
+    $trio->save();
+    return redirect("/admin/trios/{$trio->id}");
+});
+
+Route::get('/admin/trios/add', function() {
+    return view('admin.trios.add');
+});
+
+Route::post('/admin/trios/add', function(Request $request) {
+    if($request->has(['sentence1', 'sentence2', 'sentence3', 'explanation1',
+        'explanation2', 'explanation3', 'answer'])) {
+        $trio = new Trio();
+
+        $trio->sentence1 = $request->input('sentence1');
+        $trio->sentence2 = $request->input('sentence2');
+        $trio->sentence3 = $request->input('sentence3');
+        $trio->explanation1 = $request->input('explanation1');
+        $trio->explanation2 = $request->input('explanation2');
+        $trio->explanation3 = $request->input('explanation3');
+        $trio->answer = $request->input('answer');
+
+        $trio->save();
+
+        return redirect("/admin/trios/{$trio->id}");
+    } else {
+        return view('admin.trios.add')->with('trio', $request->except("_token"));
+    }
+});
