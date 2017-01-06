00402f3968955b30c3cb2be34da80bec071a5957
Denis <knopers.priv@gmail.com>
2017-01-06 23:35:54 +0100

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(TriosDevTableSeeder::class);
-        //$this->call(EntrustSeeder::class);
+        $this->call(TriosDevTableSeeder::class);
+        $this->call(EntrustSeeder::class);
     }
 }
