5d0e8628dc8060d088edec264ec69773b71a3ea1
knopers666 <knopers.priv@gmail.com>
2017-01-07 00:44:55 +0100

    Fix seed
    Signed-off-by: knopers666 <knopers.priv@gmail.com>

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,7 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(TriosDevTableSeeder::class);
-        //$this->call(EntrustSeeder::class);
+        $this->call('TriosDevTableSeeder');
+        $this->call('EntrustSeeder');
     }
 }
