f7500299ece6a417b296553546204dd4afef0f0c
MichalDolata <dev@dolata.me>
2017-01-07 19:38:52 +0100

    Add importing to DB Change files names #31

--- app/Console/Commands/ImportTrios.php
+++ app/Console/Commands/ImportTrios.php
@@ -9,6 +9,10 @@ class ImportTrios extends Command
 {
     protected $error = [];
     protected $answers = [];
+    protected $currentAnswer = 0;
+    protected $emptyFieldChar = '$';
+    protected $emptyFieldString = '';
+
     /**
      * The name and signature of the console command.
      *
@@ -31,6 +35,7 @@ class ImportTrios extends Command
     public function __construct()
     {
         parent::__construct();
+        $this->emptyFieldString = str_repeat($this->emptyFieldChar, 7);
     }
 
     /**
@@ -40,8 +45,8 @@ class ImportTrios extends Command
      */
     public function handle()
     {
-        $this->parseSentences();
         $this->parseAnswers();
+        $this->parseSentences();
 
         $this->info("Couldn't parse following files: ");
         $this->info(join("\n", $this->error));
@@ -66,14 +71,36 @@ class ImportTrios extends Command
     private function parseSentencesFile($path)
     {
         $file = file_get_contents($path);
+        // Usuwam znaki nowych lini - pomocne pozniej
         $file = str_replace("\n", "", $file);
-        $file = preg_replace("/[\. ]{5,}/", " $$$$$$$$ ", $file);
+        // Zamieniam kropki na podany znak
+        $file = preg_replace("/[\. ]{5,}/", " $this->emptyFieldString ", $file);
+        // Dodaje znak nowej linii przed każdym zdaniem, tj. przed '1.' '2.' '3.' + mała korekta bledow OCR (np. 'l.')
+        // nie działa jeśli OCR zjadł kropkę
         $file = preg_replace("/(([l123]\.)|([l123][1]))/", "\n$1", $file);
+        // korekta błedu traktuje literę l na końcu zdania jako licznik przed nowym, ten regexpr to naprawia
         $file = preg_replace("/([^\. ])\n(l{1,2}\.)/", "$1$2", $file);
 
+        // odczytanie zdania
         $matches = preg_match_all("/(([l123]\.)|([l123][1l]))([^\n]+)/", $file, $trios, PREG_SET_ORDER);
+
+        // Jeśli liczba zdań na stronie się nie zgadza to ignoruje wszystkie trios z tej strony
         if($matches !== 12) {
             $this->error[] = basename($path);
+            $this->currentAnswer += 4;
+        } else {
+            // W przeciwnym wypadku dodaje do bazy danych
+            for($i = 0; $i < 4; $i++) {
+                $newTrio = new Trio();
+                $newTrio->sentence1 = trim($trios[$i * 3][4]);
+                $newTrio->sentence2 = trim($trios[$i * 3 + 1][4]);
+                $newTrio->sentence3 = trim($trios[$i * 3 + 2][4]);
+                $newTrio->explanation1 = $newTrio->explanation2 = $newTrio->explanation3 = $this->currentAnswer;
+                $newTrio->answer = trim(strtolower($this->answers[$this->currentAnswer]));
+                $newTrio->save();
+
+                $this->currentAnswer++;
+            }
         }
     }
 
