0a380eeeeb70766e07be06311f997b7222020b70
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2017-01-13 22:02:17 +0100

    Add compiling scss to css

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,14 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-            <div class="jumbotron">
-                <p class="center-block text-center">
-                    Trios is a simple exercise to test your english skills:<br>
-                    You should put in a single word that fits in all three sentences.<br>
-                    Have fun!
-                </p>
-                <a class="btn btn-lg btn-success center-block" href="{{ action('SolveController@getRandom') }}">
-                    Start solving!
-                </a>
-            </div>
+  <section class="home">
+
+    @yield('content')
+    @include('includes.trio-logo')
+
+    <div>
+      <p class="center-block text-center home--info">
+          Trios is a simple exercise to test your english skills.<br>
+          You should put in a single word that fits in all three sentences.<br>
+          Have fun!
+      </p>
+      <a href="{{ action('SolveController@getRandom') }}" class="btn btn-success btn--home text-uppercase">Play</a>
+      <a href="{{ url('/login') }}" class="btn btn--home btn--login text-uppercase">Login</a>
+    </div>
+  </section>
+
 @endsection
