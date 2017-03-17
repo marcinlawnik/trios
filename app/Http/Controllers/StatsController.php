64c23707e05e362f2e44f2f0dfeec8ca1ed571c6
MichalDolata <dev@dolata.me>
2017-03-17 18:25:31 +0100

    Add basics admin stats

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Trio;
+use App\UserTrioAttempt;
 use Illuminate\Http\Request;
 
 class StatsController extends Controller
 {
     public function index() {
-
         $stats = [
-            'triosCount' => Trio::all()->count()
+            'triosCount' => Trio::all()->count(),
+            'totalAttempts' => UserTrioAttempt::totalAttempts(),
+            'correctAnswers' => UserTrioAttempt::totalSolved(),
+            'triosSolved' => UserTrioAttempt::triosSolved(),
+            'mostSolved' => UserTrioAttempt::mostSolvedTrios(),
+            'hardest' => UserTrioAttempt::hardestTrios()
         ];
 
         return view('pages.admin.stats.index')->with('stats', $stats);
